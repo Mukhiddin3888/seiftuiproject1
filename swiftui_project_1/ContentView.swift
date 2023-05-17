f69390aabe389ace60af8c33f3eb89f92da91773
Mukhiddin <sirojiddinovm720@mail.ru>
2023-05-17 09:44:00 +0500

    Initial Commit

---
+++ swiftui_project_1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swiftui_project_1
+//
+//  Created by Abbos Bobomurodov on 17/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui_project_1/swiftui_project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_project_1App.swift
+//  swiftui_project_1
+//
+//  Created by Abbos Bobomurodov on 17/05/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
